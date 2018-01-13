d44e9410f6f6b1db6b0044cf0189635da47a957e
Mark Apinis <mapinis2019@gmail.com>
2018-01-12 19:12:15 -0500

    Navigation Finished

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,11 +4,15 @@ import { Nav, NavItem, NavLink } from 'reactstrap';
 
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import logo from "./logo.jpg";
 
 const Index = ({ children }) => (
-    <div className="App">
-        <div className="App_Nav">
-            <p className="App_Title">mapinis</p>
+    <div id="App">
+        <div id="App_Nav">
+            <img height="auto" width="200px" id="App_Nav_Image" src={logo} alt="mapinis logo" />
+            <p id="App_Nav_Title">mapinis</p>
+            <p id="App_Nav_Subtitle">Mark Apinis's Personal Site</p>
+            <a id="App_Nav_Email" href="mailto:mapinis2019@gmail.com">mapinis2019@gmail.com</a>
             <Nav vertical>
                 <NavItem>
                     <NavLink href="/">Home</NavLink>
@@ -17,14 +21,18 @@ const Index = ({ children }) => (
                     <NavLink href="/">About</NavLink>
                 </NavItem>
                 <NavItem>
+                    <NavLink href="/">Blog</NavLink>
+                </NavItem>
+                <NavItem>
                     <NavLink href="/">Programming</NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink href="/">Photography</NavLink>
                 </NavItem>
+
             </Nav>
         </div>
-        <div className="content">
+        <div id="content">
             {children()}
         </div>
     </div>
